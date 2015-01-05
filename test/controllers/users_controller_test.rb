87d7bf8739585b5f8255feca3471ec7dae11ed41
Grant George <grantelliottgeorge@gmail.com>
2015-01-04 23:33:56 -0500

    yee haw

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
 end
